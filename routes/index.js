080adb3f23100fcf84f5ae550545f6532a5137c1
yangjing <yangjingpuyu@aliyun.com>
2018-12-13 17:25:48 +0800

    U•ェ•*U koa-router 配置

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const router = require('koa-router')({
+    prefix: '/api'
+})
+
+router.get('/demo', (ctx, next) => {
+  ctx.body = {
+    success: 'ok',
+    state: 200,
+    data: [
+      {
+        age: 20,
+        name: 'hnjnjjjj'
+      }
+    ]
+  }
+})
+
+router.get('/', (ctx, next) => {
+  console.log(ctx)
+  ctx.body = {
+    success: 'ok',
+    state: 200,
+    data: 'hello'
+  }
+})
+
+module.exports = router
